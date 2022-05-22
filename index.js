279009a541307711106ab9b0fe7bc2d593b06c0e
ParvesPR <parves2585@gmail.com>
2022-05-22 23:37:36 +0800

    find product by id

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 5000;
 require('dotenv').config();
 const app = express();
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // MIDDLEWARE
 app.use(cors());
@@ -13,7 +13,34 @@ app.use(express.json());
 // CONNECT TO MONGO DB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ey7au.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log('DB connected')
+console.log('DB connected');
+
+async function run() {
+    try {
+        await client.connect();
+
+        const productCollection = client.db('manufacturedb').collection('products');
+
+        // GET ALL DATA
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        });
+
+        // GET ITEM BY PRODUCT ID
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        });
+    }
+
+    finally { }
+};
+run().catch(console.dir)
 
 
 
